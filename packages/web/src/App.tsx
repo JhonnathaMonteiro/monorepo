fc1f20254f0e61655d95b998f5668f17165a8b48
JhonnathaMonteiro <jhonnatha.am@gmail.com>
2020-07-23 18:28:13 -0300

    changes to web

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,16 +1,28 @@
-import React, { useEffect } from 'react'
-import api from '@omnirepo/axios-config'
+import React from 'react'
+import { ThemeProvider, DefaultTheme } from 'styled-components'
+import usePeristedState from './utils/usePersistedState'
 
-// import { Container } from './styles';
+import light from './styles/themes/light'
+import dark from './styles/themes/dark'
+
+import GlobalStyle from './styles/global'
+import Header from './components/Header'
 
 const App: React.FC = () => {
-  useEffect(() => {
-    api.get('/').then(response => {
-      console.log(response)
-    })
-  }, [])
+  const [theme, setTheme] = usePeristedState<DefaultTheme>('theme', light)
 
-  return <h1>Hello World</h1>
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light)
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <GlobalStyle />
+        <Header toggleTheme={toggleTheme} />
+      </div>
+    </ThemeProvider>
+  )
 }
 
 export default App
